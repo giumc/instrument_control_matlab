0a5dd94c7332239cb0b813142315cf9cf9f81072
GabboTheBrave <79946669+GabboTheBrave@users.noreply.github.com>
2021-05-25 19:24:28 -0400

    Delete ModOff.m

--- ModOff.m
+++
@@ -1,9 +0,0 @@
-function ModOff(obj)
-% Stops the carrier modulation
-    fwrite(obj,'MODL 0');
-    
-    if str2num(query(obj,'MODL?')) == 0
-        sprintf('Modulation disabled')
-    else
-        sprintf('Modulation not disabled')
-end
