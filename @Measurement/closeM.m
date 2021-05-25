afd719349b48f5235317eee6ec958e5b711511ea
GabboTheBrave <79946669+GabboTheBrave@users.noreply.github.com>
2021-05-25 19:13:19 -0400

    Create closeM.m

---
+++ @Measurement/closeM.m
@@ -0,0 +1,10 @@
+function closeM(obj)
+
+%Closes and deletes the interface objects.
+    fclose(obj.osc.interfObj);
+    delete(obj.osc.interfObj);
+    fclose(obj.tek.interfObj);
+    delete(obj.tek.interfObj);
+    fclose(obj.mod.interfObj);
+    delete(obj.mod.interfObj);
+end
