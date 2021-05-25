460a2740448c7533423835f6957a4020fe174a36
GabboTheBrave <79946669+GabboTheBrave@users.noreply.github.com>
2021-05-25 19:18:54 -0400

    Delete ModOn.m

--- ModOn.m
+++
@@ -1,8 +0,0 @@
-function ModOn(obj)
-    fwrite(obj,'MODL 1');
-    
-    if str2num(query(obj,'MODL?')) == 1
-        sprintf('Modulation enabled')
-    else
-        sprintf('Modulation not enabled')
-end
